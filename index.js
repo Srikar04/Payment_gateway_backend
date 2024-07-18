3763d3de27d63ce5394732bc270be3f8746ff9e0
srikar <msrikar0403@gmail.com>
2024-07-18 23:27:33 +0530

    application binding

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use("/", (req, res, next) => {
 app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Server is running on ${PORT}`);
 });
